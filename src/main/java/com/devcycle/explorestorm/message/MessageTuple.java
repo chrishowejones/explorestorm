1423db2a7e7e3264c3a62f6f801b43b6883b0291
Chris Howe-Jones <chris.howejones@gmail.com>
2015-10-21 15:43:08 +0100

    Implement getValues & add to getFields.

--- src/main/java/com/devcycle/explorestorm/message/MessageTuple.java
+++ src/main/java/com/devcycle/explorestorm/message/MessageTuple.java
@@ -1,6 +1,7 @@
 package com.devcycle.explorestorm.message;
 
 import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +18,7 @@ public class MessageTuple {
     private final boolean empty;
     private Logger LOG = LoggerFactory.getLogger(MessageTuple.class);
     private Fields fields;
+    private Values values;
 
     /**
      * Creates a MessageTuple from a Map of keys that represent Fields and Objects that represent Values.
@@ -24,6 +26,10 @@ public class MessageTuple {
      * @param fieldValueMap - map of field names to object values that will be encapsulated by this tuple.
      */
     public MessageTuple(Map<String, Object> fieldValueMap) {
+        if (fieldValueMap == null)
+            throw new IllegalArgumentException("Field Value Map cannot be null.");
+        if (fieldValueMap.isEmpty())
+            throw new IllegalArgumentException("Field Value Map cannot be empty");
         LOG.trace("Created MessageTuple from map");
         empty = fieldValueMap.isEmpty();
         loadKeyValues(fieldValueMap);
@@ -31,13 +37,29 @@ public class MessageTuple {
 
     private void loadKeyValues(Map<String, Object> fieldValueMap) {
         fields = new Fields(new ArrayList(fieldValueMap.keySet()));
+        values = new Values();
+        values.addAll(fieldValueMap.values());
     }
 
+    /**
+     * Checks if this message tuple is empty (has no Fields or Values).
+     *
+     * @return true if empty, false otherwise.
+     */
     public boolean isEmpty() {
         return empty;
     }
 
+    /**
+     * Returns Fields for this tuple.
+     *
+     * @return Fields in this tuple.
+     */
     public Fields getFields() {
         return fields;
+    }
+
+    public Values getValues() {
+        return values;
     }
 }
--- src/test/java/com/devcycle/explorestorm/message/MessageTupleTest.java
+++ src/test/java/com/devcycle/explorestorm/message/MessageTupleTest.java
@@ -1,10 +1,17 @@
 package com.devcycle.explorestorm.message;
 
+import backtype.storm.tuple.Values;
+import org.apache.commons.digester.Rules;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -15,13 +22,8 @@ import static org.junit.Assert.fail;
  */
 public class MessageTupleTest {
 
-    @Test
-    public void testCreateMessageTupleFromEmptyMap() {
-        Map<String,Object> fieldValuesMap = new HashMap<String, Object>();
-        MessageTuple tuple = new MessageTuple(fieldValuesMap);
-        assertThat(tuple, notNullValue());
-        assertThat(tuple.isEmpty(), is(true));
-    }
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void testCreateMessageTupleFromPopulatedMap() {
@@ -35,14 +37,52 @@ public class MessageTupleTest {
     @Test
     public void testGetFields() {
         Map<String, Object> fieldValuesMap = new HashMap<>();
-        String test = "test";
-        fieldValuesMap.put(test, "test message");
-        String again = "again";
-        fieldValuesMap.put(again, "another message");
+        final String test = "test";
+        final String value = "test message";
+        fieldValuesMap.put(test, value);
+        final String again = "again";
+        final String value1 = "another message";
+        fieldValuesMap.put(again, value1);
         MessageTuple tuple = new MessageTuple(fieldValuesMap);
         assertThat(tuple.getFields(), notNullValue());
+        assertThat(tuple.getFields().size(), is(2));
         assertThat(tuple.getFields().contains(test), is(true));
         assertThat(tuple.getFields().contains(again), is(true));
+        assertThat(tuple.getFields().get(0), is(test));
+        assertThat(tuple.getFields().get(1), is(again));
+    }
+
+    @Test
+    public void testGetValues() {
+        Map<String, Object> fieldValuesMap = new HashMap<>();
+        final String test = "test";
+        final String testValue = "test message";
+        fieldValuesMap.put(test, testValue);
+        final String anotherMessage = "another message";
+        final String again = "again";
+        fieldValuesMap.put(again, anotherMessage);
+
+        MessageTuple tuple = new MessageTuple(fieldValuesMap);
+        assertThat(tuple.getValues(), notNullValue());
+        assertThat(tuple.getValues().size(), is(2));
+        assertThat(tuple.getValues().contains(testValue), is(true));
+        assertThat(tuple.getValues().contains(anotherMessage), is(true));
+        assertThat((String)tuple.getValues().get(0), is(testValue));
+        assertThat((String)tuple.getValues().get(1), is(anotherMessage));
+    }
+
+    @Test
+    public void throwsIllegalArgumentExceptionIfNullMapConstructor() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage(containsString("null"));
+        new MessageTuple(null);
+     }
+
+    @Test
+    public void throwsIllegalArgumentExceptionIfEmptyMapConstructor() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage(containsString("empty"));
+        new MessageTuple(new HashMap<String, Object>());
     }
 
 }
